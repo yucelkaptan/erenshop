4afe9a57e5aa19ad0aee3bb67e5d9d746532170a
yucelkaptan <127623528+yucelkaptan@users.noreply.github.com>
2024-04-18 14:23:30 +0300

    erenshopv11-duzeltme2

--- src/App.js
+++ src/App.js
@@ -16,49 +16,58 @@ import TermsofUsePage from './router/TermsofUsePage/TermsofUsePage';
 
 const App = () => {
 
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
     const toggleMenu = () => {
       setIsOpen(!isOpen); 
+    };
+
+    const handleMouseEnter = () => {
+      setIsDropdownOpen(true);
+    };
+    
+    const handleMouseLeave = () => {
+      setIsDropdownOpen(false);
     };
 
   return (
     <>
         <Router>
         <nav className="navbar navbar-expand-lg bg-body-tertiary changing">
-      <div className="container-fluid">
+    <div className="container-fluid">
       <a href="/" className="navbar-brand">
-            <img src={logo} alt="ErenShop Logo" width={250}/>
-          </a>
-        <button className="navbar-toggler" type="button" onClick={toggleMenu}>
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link to="/" className="nav-link active" aria-current="page">Anasayfa</Link>
-            </li>
-            <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Ürünler
-                  </a>
-                  <ul className="dropdown-menu">
-                  <li>
-                    <Link className="dropdown-item" to="/Elbise">Elbiseler</Link>
-                    <Link className="dropdown-item" to="/Gomlek">Gömlek</Link>
-                  </li>
-                </ul>
-            </li>
-            <li className="nav-item">
-              <Link to="/Hakkimizda" className="nav-link active">Hakkımızda</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/iletişim" className="nav-link active">İletişim</Link>
-            </li>
-          </ul>
-        </div>
+        <img src={logo} alt="ErenShop Logo" width={250} />
+      </a>
+      <button className="navbar-toggler" type="button" onClick={toggleMenu}>
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <Link to="/" className="nav-link active" aria-current="page">Anasayfa</Link>
+          </li>
+          <li className="nav-item dropdown" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <a className="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Ürünler 
+            </a>
+            <ul className={`dropdown-menu ${isDropdownOpen ? 'show' : ''}`}>
+              <li>
+                <Link className="dropdown-item" to="/Elbise">Elbiseler</Link>
+                <Link className="dropdown-item" to="/Gomlek">Gömlek</Link>
+              </li>
+            </ul>
+          </li>
+          <li className="nav-item">
+            <Link to="/Hakkimizda" className="nav-link active">Hakkımızda</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/iletişim" className="nav-link active">İletişim</Link>
+          </li>
+        </ul>
       </div>
-    </nav>
+    </div>
+  </nav>
 
         <div className="container mt-4">
           <Routes>
